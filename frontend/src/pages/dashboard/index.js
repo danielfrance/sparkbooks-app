a0251597f98a738ee5968a31babe3e946270359a
Herve R. Munyurangeri <rherve@gmail.com>
2023-03-19 10:41:47 -0500

    Fix dashboard header

--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -1,20 +1,139 @@
-import AppLayout from "@/components/Layouts/AppLayout";
+import AppLayout from '@/components/Layouts/AppLayout'
 import {
-  Box,
-  DataTable,
-  Meter,
-  Heading,
-  Text,
-  Grid,
-  Card,
-  CardBody,
-} from "grommet";
-import { DocumentStore, DocumentVerified } from "grommet-icons";
+    Box,
+    DataTable,
+    Meter,
+    Heading,
+    Text,
+    Grid,
+    Card,
+    CardBody,
+} from 'grommet'
+import { DocumentStore, DocumentVerified } from 'grommet-icons'
 
 export default function Dashboard() {
-  return (
-      <AppLayout>
-          <Grid
+    return (
+        <AppLayout>
+            <div className="flex dashboard-header">
+                <div className="flex status bg-dark text-white">
+                    <img src="/processed.png" />
+                    <div>
+                        <span className="fs-700">120</span>
+                        <span className="fs-100">Files processed</span>
+                    </div>
+                </div>
+                <div className="flex status bg-dark text-white">
+                    <img src="/pending.png" />
+                    <div>
+                        <span className="fs-700">880</span>
+                        <span className="fs-100">Files remaining</span>
+                        <span
+                            className="fs-100"
+                            style={{
+                                marginTop: '-0.313rem',
+                                textDecoration: 'underline',
+                            }}>
+                            Upgrade
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+            <Box className="box_container" fill>
+                <Box direction="row" justify="between">
+                    <Heading margin="none" level="3" color="brand">
+                        Recent Uploads
+                    </Heading>
+                </Box>
+                <DataTable
+                    sortable
+                    margin={{ top: '3em' }}
+                    paginate={{ step: 10 }}
+                    alignSelf="stretch"
+                    columns={[
+                        {
+                            property: 'id',
+                            header: <Text>ID</Text>,
+                            primary: true,
+                        },
+                        {
+                            property: 'client',
+                            header: <Text>Client</Text>,
+                        },
+                        {
+                            property: 'files',
+                            header: <Text>Files</Text>,
+                        },
+                        {
+                            property: 'percent',
+                            size: 'medium',
+                            header: 'Processing %',
+                            render: datum => (
+                                <Box
+                                    pad={{ vertical: 'xsmall' }}
+                                    direction="row">
+                                    <Meter
+                                        values={[
+                                            {
+                                                value: datum.percent,
+                                                color: 'brand',
+                                                background: 'primary',
+                                            },
+                                        ]}
+                                        thickness="small"
+                                        size="small"
+                                    />
+                                    <Text
+                                        size="small"
+                                        margin={{ left: '20px' }}>
+                                        {datum.percent}%
+                                    </Text>
+                                </Box>
+                            ),
+                        },
+                    ]}
+                    data={[
+                        { id: '1', client: 'Alan', files: 23, percent: 20 },
+                        { id: '2', client: 'Bryan', files: 3, percent: 30 },
+                        { id: '3', client: 'Chris', files: 11, percent: 40 },
+                        { id: '4', client: 'Eric', files: 13, percent: 100 },
+                        { id: '1', client: 'Alan', files: 23, percent: 20 },
+                        { id: '2', client: 'Bryan', files: 3, percent: 30 },
+                        { id: '3', client: 'Chris', files: 11, percent: 40 },
+                        { id: '4', client: 'Eric', files: 13, percent: 100 },
+                        { id: '1', client: 'Alan', files: 23, percent: 20 },
+                        { id: '2', client: 'Bryan', files: 3, percent: 30 },
+                        { id: '3', client: 'Chris', files: 11, percent: 40 },
+                        { id: '4', client: 'Eric', files: 13, percent: 100 },
+                        { id: '1', client: 'Alan', files: 23, percent: 20 },
+                        { id: '2', client: 'Bryan', files: 3, percent: 30 },
+                        { id: '3', client: 'Chris', files: 11, percent: 40 },
+                        { id: '4', client: 'Eric', files: 13, percent: 100 },
+                        { id: '1', client: 'Alan', files: 23, percent: 20 },
+                        { id: '2', client: 'Bryan', files: 3, percent: 30 },
+                        { id: '3', client: 'Chris', files: 11, percent: 40 },
+                        { id: '4', client: 'Eric', files: 13, percent: 100 },
+                        { id: '1', client: 'Alan', files: 23, percent: 20 },
+                        { id: '2', client: 'Bryan', files: 3, percent: 30 },
+                        { id: '3', client: 'Chris', files: 11, percent: 40 },
+                        { id: '4', client: 'Eric', files: 13, percent: 100 },
+                        { id: '1', client: 'Alan', files: 23, percent: 20 },
+                        { id: '2', client: 'Bryan', files: 3, percent: 30 },
+                        { id: '3', client: 'Chris', files: 11, percent: 40 },
+                        { id: '4', client: 'Eric', files: 13, percent: 100 },
+                        { id: '1', client: 'Alan', files: 23, percent: 20 },
+                        { id: '2', client: 'Bryan', files: 3, percent: 30 },
+                        { id: '3', client: 'Chris', files: 11, percent: 40 },
+                        { id: '4', client: 'Eric', files: 13, percent: 100 },
+                    ]}
+                />
+            </Box>
+        </AppLayout>
+    )
+}
+
+{
+    /* <Grid
               alignSelf="stretch"
               rows={['auto', 'flex']}
               pad="medium"
@@ -68,92 +187,5 @@ export default function Dashboard() {
                       </Grid>
                   </CardBody>
               </Card>
-          </Grid>
-          <Box className="box_container" fill>
-              <Box direction="row" justify="between">
-                  <Heading margin="none" level="3" color="brand">
-                      Recent Uploads
-                  </Heading>
-              </Box>
-              <DataTable
-                  sortable
-                  margin={{ top: '3em' }}
-                  paginate={{ step: 10 }}
-                  alignSelf="stretch"
-                  columns={[
-                      {
-                          property: 'id',
-                          header: <Text>ID</Text>,
-                          primary: true,
-                      },
-                      {
-                          property: 'client',
-                          header: <Text>Client</Text>,
-                      },
-                      {
-                          property: 'files',
-                          header: <Text>Files</Text>,
-                      },
-                      {
-                          property: 'percent',
-                          size: 'medium',
-                          header: 'Processing %',
-                          render: datum => (
-                              <Box pad={{ vertical: 'xsmall' }} direction="row">
-                                  <Meter
-                                      values={[
-                                          {
-                                              value: datum.percent,
-                                              color: 'brand',
-                                              background: 'primary',
-                                          },
-                                      ]}
-                                      thickness="small"
-                                      size="small"
-                                  />
-                                  <Text size="small" margin={{ left: '20px' }}>
-                                      {datum.percent}%
-                                  </Text>
-                              </Box>
-                          ),
-                      },
-                  ]}
-                  data={[
-                      { id: '1', client: 'Alan', files: 23, percent: 20 },
-                      { id: '2', client: 'Bryan', files: 3, percent: 30 },
-                      { id: '3', client: 'Chris', files: 11, percent: 40 },
-                      { id: '4', client: 'Eric', files: 13, percent: 100 },
-                      { id: '1', client: 'Alan', files: 23, percent: 20 },
-                      { id: '2', client: 'Bryan', files: 3, percent: 30 },
-                      { id: '3', client: 'Chris', files: 11, percent: 40 },
-                      { id: '4', client: 'Eric', files: 13, percent: 100 },
-                      { id: '1', client: 'Alan', files: 23, percent: 20 },
-                      { id: '2', client: 'Bryan', files: 3, percent: 30 },
-                      { id: '3', client: 'Chris', files: 11, percent: 40 },
-                      { id: '4', client: 'Eric', files: 13, percent: 100 },
-                      { id: '1', client: 'Alan', files: 23, percent: 20 },
-                      { id: '2', client: 'Bryan', files: 3, percent: 30 },
-                      { id: '3', client: 'Chris', files: 11, percent: 40 },
-                      { id: '4', client: 'Eric', files: 13, percent: 100 },
-                      { id: '1', client: 'Alan', files: 23, percent: 20 },
-                      { id: '2', client: 'Bryan', files: 3, percent: 30 },
-                      { id: '3', client: 'Chris', files: 11, percent: 40 },
-                      { id: '4', client: 'Eric', files: 13, percent: 100 },
-                      { id: '1', client: 'Alan', files: 23, percent: 20 },
-                      { id: '2', client: 'Bryan', files: 3, percent: 30 },
-                      { id: '3', client: 'Chris', files: 11, percent: 40 },
-                      { id: '4', client: 'Eric', files: 13, percent: 100 },
-                      { id: '1', client: 'Alan', files: 23, percent: 20 },
-                      { id: '2', client: 'Bryan', files: 3, percent: 30 },
-                      { id: '3', client: 'Chris', files: 11, percent: 40 },
-                      { id: '4', client: 'Eric', files: 13, percent: 100 },
-                      { id: '1', client: 'Alan', files: 23, percent: 20 },
-                      { id: '2', client: 'Bryan', files: 3, percent: 30 },
-                      { id: '3', client: 'Chris', files: 11, percent: 40 },
-                      { id: '4', client: 'Eric', files: 13, percent: 100 },
-                  ]}
-              />
-          </Box>
-      </AppLayout>
-  )
+          </Grid> */
 }
