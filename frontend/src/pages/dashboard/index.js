df4d5cdad824f3388ef5cee27a999c6747e9f76d
Herve R. Munyurangeri <rherve@gmail.com>
2023-03-19 11:01:18 -0500

    Make Dashboard responsive
    	new file:   src/components/Layouts/DataTable.js
    	modified:   src/pages/dashboard/index.js
    	modified:   src/styles/globals.css

--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -1,17 +1,272 @@
 import AppLayout from '@/components/Layouts/AppLayout'
-import {
-    Box,
-    DataTable,
-    Meter,
-    Heading,
-    Text,
-    Grid,
-    Card,
-    CardBody,
-} from 'grommet'
-import { DocumentStore, DocumentVerified } from 'grommet-icons'
+import { Box, Meter, Text, Avatar } from 'grommet'
+import DataTable from '@/components/Layouts/DataTable'
+import { useState } from 'react'
+import { useUIContext } from '@/contexts/ui'
+
+const src = '//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80'
+
+const clientRender = datum => (
+    <Box pad={{ vertical: 'xsmall' }} gap="small" direction="row">
+        {/* <Avatar size="small" round="xsmall" src={src} /> */}
+        <Text>{datum.client}</Text>
+    </Box>
+)
+
+const processingDataRender = datum => (
+    <Box pad={{ vertical: 'xsmall' }} direction="row">
+        <Meter
+            values={[
+                {
+                    value: datum.percent,
+                    color: '#4112fb',
+                    background: '#ECECEC',
+                },
+            ]}
+            thickness="xsmall"
+            round
+        />
+        <Text className="text-dark" margin={{ left: '20px' }}>
+            {datum.percent}%
+        </Text>
+    </Box>
+)
+
+const columns = [
+    {
+        property: 'id',
+        header: <Text>Uploads</Text>,
+        size: 'small',
+        // sortable: true,
+        // primary: true,
+    },
+    {
+        property: 'client',
+        size: 'medium',
+        header: <Text>Client</Text>,
+        render: clientRender,
+    },
+    {
+        property: 'files',
+        size: 'medium',
+        header: <Text>Files</Text>,
+    },
+    {
+        property: 'percent',
+        size: 'medium',
+        header: <Text>Processing %</Text>,
+        render: processingDataRender,
+    },
+]
+const data = [
+    {
+        id: '1',
+        client: 'Alan',
+        files: 23,
+        percent: 20,
+    },
+    {
+        id: '2',
+        client: 'Bryan',
+        files: 3,
+        percent: 30,
+    },
+    {
+        id: '3',
+        client: 'Chris',
+        files: 11,
+        percent: 40,
+    },
+    {
+        id: '4',
+        client: 'Eric',
+        files: 13,
+        percent: 100,
+    },
+    {
+        id: '1',
+        client: 'Alan',
+        files: 23,
+        percent: 20,
+    },
+    {
+        id: '2',
+        client: 'Bryan',
+        files: 3,
+        percent: 30,
+    },
+    {
+        id: '3',
+        client: 'Chris',
+        files: 11,
+        percent: 40,
+    },
+    {
+        id: '4',
+        client: 'Eric',
+        files: 13,
+        percent: 100,
+    },
+    {
+        id: '1',
+        client: 'Alan',
+        files: 23,
+        percent: 20,
+    },
+    {
+        id: '2',
+        client: 'Bryan',
+        files: 3,
+        percent: 30,
+    },
+    {
+        id: '3',
+        client: 'Chris',
+        files: 11,
+        percent: 40,
+    },
+    {
+        id: '4',
+        client: 'Eric',
+        files: 13,
+        percent: 100,
+    },
+    {
+        id: '1',
+        client: 'Alan',
+        files: 23,
+        percent: 20,
+    },
+    {
+        id: '2',
+        client: 'Bryan',
+        files: 3,
+        percent: 30,
+    },
+    {
+        id: '3',
+        client: 'Chris',
+        files: 11,
+        percent: 40,
+    },
+    {
+        id: '4',
+        client: 'Eric',
+        files: 13,
+        percent: 100,
+    },
+    {
+        id: '1',
+        client: 'Alan',
+        files: 23,
+        percent: 20,
+    },
+    {
+        id: '2',
+        client: 'Bryan',
+        files: 3,
+        percent: 30,
+    },
+    {
+        id: '3',
+        client: 'Chris',
+        files: 11,
+        percent: 40,
+    },
+    {
+        id: '4',
+        client: 'Eric',
+        files: 13,
+        percent: 100,
+    },
+    {
+        id: '1',
+        client: 'Alan',
+        files: 23,
+        percent: 20,
+    },
+    {
+        id: '2',
+        client: 'Bryan',
+        files: 3,
+        percent: 30,
+    },
+    {
+        id: '3',
+        client: 'Chris',
+        files: 11,
+        percent: 40,
+    },
+    {
+        id: '4',
+        client: 'Eric',
+        files: 13,
+        percent: 100,
+    },
+    {
+        id: '1',
+        client: 'Alan',
+        files: 23,
+        percent: 20,
+    },
+    {
+        id: '2',
+        client: 'Bryan',
+        files: 3,
+        percent: 30,
+    },
+    {
+        id: '3',
+        client: 'Chris',
+        files: 11,
+        percent: 40,
+    },
+    {
+        id: '4',
+        client: 'Eric',
+        files: 13,
+        percent: 100,
+    },
+    {
+        id: '1',
+        client: 'Alan',
+        files: 23,
+        percent: 20,
+    },
+    {
+        id: '2',
+        client: 'Bryan',
+        files: 3,
+        percent: 30,
+    },
+    {
+        id: '3',
+        client: 'Chris',
+        files: 11,
+        percent: 40,
+    },
+    {
+        id: '4',
+        client: 'Eric',
+        files: 13,
+        percent: 100,
+    },
+]
 
 export default function Dashboard() {
+    const [selected, setSelected] = useState()
+    const onClickRow = datum => console.log(datum)
+    const actions = [
+        // { label: 'Add', onClick: e => console.log(e) },
+        // { label: 'Edit', onClick: e => console.log(e) },
+    ]
+
+    const { filterQuery } = useUIContext()
+
+    const filtered = data.filter(datum =>
+        datum.client.toLocaleLowerCase().includes(filterQuery),
+    )
+
     return (
         <AppLayout>
             <div className="flex dashboard-header">
@@ -39,7 +294,20 @@ export default function Dashboard() {
                 </div>
             </div>
 
-            <Box className="box_container" fill>
+            <DataTable
+                title="Recent Uploads"
+                columns={columns}
+                data={filtered}
+                setSelected={setSelected}
+                onClickRow={onClickRow}
+                actions={actions}
+            />
+        </AppLayout>
+    )
+}
+
+{
+    /* <Box className="box_container" fill>
                 <Box direction="row" justify="between">
                     <Heading margin="none" level="3" color="brand">
                         Recent Uploads
@@ -127,9 +395,7 @@ export default function Dashboard() {
                         { id: '4', client: 'Eric', files: 13, percent: 100 },
                     ]}
                 />
-            </Box>
-        </AppLayout>
-    )
+            </Box> */
 }
 
 {
