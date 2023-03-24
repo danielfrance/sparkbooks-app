2453c62a74bbf62ffc3eb4e9529c09295fe50bff
Daniel France <dan@ittybam.com>
2023-03-24 15:34:38 -0500

    uploads working

--- frontend/src/components/Layouts/UploadFilesLayer.js
+++ frontend/src/components/Layouts/UploadFilesLayer.js
@@ -18,8 +18,8 @@ import { useUIContext } from '@/contexts/ui'
 import axios from '@/lib/axios'
 
 const UploadFilesLayer = ({ client, isOpen, onClose }) => {
-    const [selectedClient, setSelectedClient] = useState()
-    const [selectedFiles, setSelectedFiles] = useState([])
+    const [selectedClient, setSelectedClient] = useState([])
+    const [selectedFiles, setSelectedFiles] = useState(null)
     const [show, setShow] = useState(false)
     // const fileInputStyles = {}
 
@@ -42,17 +42,23 @@ const UploadFilesLayer = ({ client, isOpen, onClose }) => {
                     },
                 }
 
-            const data = {
-                files: selectedFiles,
-                client_id: selectedClient,
-            }
+            const formData = new FormData()
 
-            const res = await axios.post('/upload/new', data, {
-                headers: {
-                    cookie: cookie,
-                },
+            formData.append('client_id', selectedClient.id)
+            selectedFiles.forEach(file => {
+                formData.append('files[]', file)
             })
-            console.log(res.data)
+
+            try {
+                const res = await axios.post('/upload/new', formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                    },
+                })
+                console.log(res.data)
+            } catch (error) {
+                console.log(error)
+            }
             setShow(false)
             onClose()
         }
