5f1cedfe4dbb9a6a2e99af1a66d267dd0f1fa24a
Daniel France <dan@ittybam.com>
2023-04-07 19:00:45 -0500

    added softdeletes to users

--- sparkbooks-server/app/Models/User.php
+++ sparkbooks-server/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -12,7 +13,7 @@ use Laratrust\Traits\LaratrustUserTrait;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, LaratrustUserTrait;
+    use HasApiTokens, HasFactory, Notifiable, LaratrustUserTrait, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
