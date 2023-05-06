1054269964a4ae4ae536a0d2e2f49a04afbfad85
Daniel France <dan@ittybam.com>
2023-05-06 15:36:53 -0500

    migration updates

--- sparkbooks-server/deployments/run.sh
+++ sparkbooks-server/deployments/run.sh
@@ -14,18 +14,13 @@ if [ "$APP_ENV" = "local" ] || [ "$APP_ENV" = "demo" ]; then
     echo "🔥 Running Seeder"
     php artisan db:seed --class=DatabaseSeeder
     echo "🔥 Dump Autoloader & Install Dependencies"
-    composer dumpautoload
-    composer install
 fi
 
 if [ "$APP_ENV" = "prod" ]; then
     echo "🔥 Running Production Seeder"
     php artisan db:seed --class=ProductionSeeder --force
     echo "🔥 Dump Autoloader & Install Dependencies"
-    composer dumpautoload
-    composer install
 fi
 
 echo "🔥 starting supervisord"
 /usr/bin/supervisord -c /etc/supervisord.conf
-
